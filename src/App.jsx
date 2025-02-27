b5625544f982e24fad67bcab67d613ae58394e95
mirzawajihali <mirzawajihali00@gmail.com>
2025-02-27 19:29:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h2 className='font-bold text-9xl underline bg-amber-50 '>hello world</h2>
+    </>
+  )
+}
+
+export default App
